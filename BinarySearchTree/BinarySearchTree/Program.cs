618e1d185c5aa7ef50f903101a958b67b3e40666
Muhammad Qasim Pasta <mqpasta@gmail.com>
2019-01-01 19:28:13 +0500

    dijsktra, linkedlist and bst
    Added solutions of LinkedList, Binary Search Tree and Dijsktra

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Add(20);
+            bst.Add(10);
+            bst.Add(30);
+            bst.Add(5);
+            bst.Add(2);
+            bst.Add(15);
+            bst.Add(17);
+
+            bst.InOrder();
+            bst.Delete(30);
+            bst.InOrder();
+
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        private BNode root;
+
+        public BinarySearchTree()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            this.root = null;
+        }
+
+        public void Add(int e)
+        {
+            root = Add(root, e);
+        }
+
+        public BNode Search(int e)
+        {
+            return Search(root, e);
+        }
+
+        private BNode Search(BNode node, int e)
+        {
+            if (node == null)
+                return null;
+
+            if (node.data == e)
+                return node;
+
+            if (e < node.data)
+                return Search(node.left, e);
+            else
+                return Search(node.right, e);
+        }
+
+        private BNode Add(BNode node, int e)
+        {
+            if (node == null)
+            {
+                node = new BNode(e);
+                return node;
+            }
+
+            if (e < node.data)
+                node.left = Add(node.left, e);
+            else
+                node.right = Add(node.right, e);
+
+            return node;
+        }
+
+        public void InOrder()
+        {
+            InOrder(root);
+        }
+
+        public void PreOrder()
+        {
+            PreOder(root);
+        }
+
+        public void PostOrder()
+        {
+            PostOder(root);
+        }
+
+        private void InOrder(BNode node)
+        {
+            if (node == null)
+                return;
+            InOrder(node.left);
+            Console.WriteLine(node.data);
+            InOrder(node.right);
+        }
+
+        private void PreOder(BNode node)
+        {
+            if (node == null)
+                return;
+
+            Console.WriteLine(node.data);
+            PreOder(node.left);
+            PreOder(node.right);
+        }
+
+        private void PostOder(BNode node)
+        {
+            if (node == null)
+                return;
+
+            PostOder(node.left);
+            PostOder(node.right);
+            Console.WriteLine(node.data);
+        }
+
+        public int Height()
+        {
+            return Height(root);
+        }
+
+        public int Height(BNode node)
+        {
+            if (node == null)
+                return -1;
+
+            int l = Height(node.left);
+            int r = Height(node.right);
+
+            if (l > r)
+                return l + 1;
+            else
+                return r + 1;
+        }
+
+        public int Size()
+        {
+            return Size(root);
+        }
+
+        public int Size(BNode node)
+        {
+            if (node == null)
+                return 0;
+
+            return 1 + Size(node.left) + Size(node.right);
+        }
+
+        public BNode Successor(BNode node)
+        {
+            if (node == null)
+                return null;
+
+            return Minimum(node.right);
+        }
+
+        public BNode Predecessor(BNode node)
+        {
+            if (node == null)
+                return null;
+
+            return Maximum(node.left);
+        }
+
+        public BNode Minimum(BNode node)
+        {
+            if (node == null)
+                return null;
+
+            if (node.left == null)
+                return node;
+            else
+                return Minimum(node.left);
+        }
+
+        public BNode Maximum(BNode node)
+        {
+            if (node == null)
+                return null;
+
+            if (node.right == null)
+                return node;
+            else
+                return Maximum(node.right);
+        }
+
+        public void Delete(int e)
+        {
+            root = Delete(root, e);
+        }
+
+        public BNode Delete(BNode node, int e)
+        {
+            if (node == null)
+                return null;
+
+            if (node.data == e)
+            {
+                // case 1
+                if (node.left == null && node.right == null)
+                    return null;
+                // case 2 - left child is not empty - connect parent with left child
+                if (node.left != null && node.right == null)
+                    return node.left;
+                // case 2 - right child is not empty - connect parent with right child
+                if (node.right != null && node.left == null)
+                    return node.right;
+                // nothing like above then
+                // both children are not empty
+                if (node.right != null)
+                {
+                    BNode succ = Successor(node);
+                    node.data = succ.data;
+                    node.right = Delete(node.right, succ.data);
+                }
+                else
+                {
+                    BNode pred = Predecessor(node);
+                    node.data = pred.data;
+                    node.left = Delete(node.left, pred.data);
+                }
+
+                return node;
+            }
+
+            if (e < node.data)
+                node.left = Delete(node.left, e);
+            else
+                node.right = Delete(node.right, e);
+
+            return node;
+        }
+    }
+
+    public class BNode
+    {
+        public int data;
+        public BNode left;
+        public BNode right;
+
+        public BNode()
+        {
+            Initialize();
+        }
+
+        public BNode(int e)
+        {
+            this.data = e;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            left = null;
+            right = null;
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewMethod();
+            DList list = new DList();
+            list.AddFirst(20);
+            list.AddFirst(30);
+            list.AddFirst(50);
+            list.Print();
+
+            Console.WriteLine("adding 70");
+            list.AddAfter(30, 70);
+            list.Print();
+
+
+            //DNode f = list.Search(30);
+            //if (f != null)
+            //    Console.WriteLine("Found:{0}", f.data);
+
+        }
+
+        private static void NewMethod()
+        {
+            SList list = new SList();
+            list.AddFirst(20);
+            list.AddFirst(101);
+            list.AddFirst(30);
+            list.Print();
+            list.RemoveLast();
+            Console.WriteLine("Printing after removal");
+            list.Print();
+
+            //Node f = list.Search(201);
+            //if (f != null)
+            //    Console.WriteLine("Found data is:{0}", f.data);
+            //else
+            //    Console.WriteLine("Not found");
+        }
+    }
+
+    public class SList
+    {
+        Node head;
+        Node tail;
+
+        public SList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddFirst(int e)
+        {
+            Node n = new Node(e);
+            n.next = head;
+            head = n;
+
+            if (tail == null)
+                tail = n;
+        }
+
+        public void AddLast(int e)
+        {
+            Node n = new Node(e);
+            if (tail != null)
+                tail.next = n;
+            tail = n;
+
+            if (head == null)
+                head = n;
+        }
+
+        public void RemoveFirst()
+        {
+            if (head != null)
+            {
+                Node x = head;
+                head = head.next;
+                x = null;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            Node x = head;
+            while (x.next != tail)
+            {
+                x = x.next;
+            }
+
+            tail = x;
+            tail.next = null;
+        }
+
+        public void Print()
+        {
+            Node x = head;
+            while (x != null)
+            {
+                Console.WriteLine(x.data);
+                x = x.next;
+            }
+        }
+
+        public Node Search(int e)
+        {
+            Node x = head;
+            while (x != null)
+            {
+                if (x.data == e)
+                    return x;
+                x = x.next;
+            }
+
+            return null;
+        }
+    }
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int e)
+        {
+            data = e;
+            next = null;
+        }
+    }
+
+    public class DNode
+    {
+        public int data;
+        public DNode next;
+        public DNode previous;
+
+        public DNode(int e)
+        {
+            data = e;
+            next = null;
+            previous = null;
+        }
+    }
+
+    public class DList
+    {
+        DNode head;
+        DNode tail;
+
+        public DList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void AddFirst(int e)
+        {
+            DNode n = new DNode(e);
+
+            if (head == null)
+            {
+                head = n;
+                tail = n;
+            }
+            else
+            {
+                n.next = head;
+                head.previous = n;
+                head = n;
+            }
+        }
+
+        public void AddAfter(int x, int e)
+        {
+            DNode found = Search(x);
+            DNode y = found.next;
+            DNode n = new DNode(e);
+            n.previous = found;
+            n.next = y;
+            found.next = n;
+            y.previous = n;
+        }
+
+        public void Print()
+        {
+            DNode x = head;
+            while(x != null)
+            {
+                Console.WriteLine(x.data);
+                x = x.next;
+            }
+        }
+
+        public DNode Search(int e)
+        {
+            DNode x = head;
+            while(x != null)
+            {
+                if (x.data == e)
+                    return x;
+                x = x.next;
+            }
+
+            return null;
+        }
+    }
+}
